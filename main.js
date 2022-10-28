1cbaa6fbc9ce8b4c5e784191dc0271c360a0a403
Le Huynh <lhuynh100@gmail.com>
2022-10-28 16:26:12 -0500

    added elements of documents

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 //base URL
 const baseURL = 'http://localhost:3000/cards/'
 
+//grab elements of document
+const main = document.getElementById('main')
+const viewCollection = document.getElementById('view')
+const newCard = document.getElementById('newCard')
