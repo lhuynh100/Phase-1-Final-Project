6bc686fe2cca24102568c1a41d4218ff8d9ab0cf
Le Huynh <lhuynh100@gmail.com>
2022-10-28 16:30:06 -0500

    DOMContentLoaded function

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const baseURL = 'http://localhost:3000/cards/'
 const main = document.getElementById('main')
 const viewCollection = document.getElementById('view')
 const newCard = document.getElementById('newCard')
+
+//content load and page refresh
+document.addEventListener('DOMContentLoaded',()=>{
+    
+ 
+})
